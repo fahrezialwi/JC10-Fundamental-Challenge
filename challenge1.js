088c67ccf8de78ab06d84a1a16847363137ce265
fahrezialwi <fahrezialwi@gmail.com>
2019-08-11 15:40:08 +0700

    Fix rotating function

--- challenge1.js
+++ challenge1.js
@@ -1,53 +1,51 @@
 let arrayAngka = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
 ]
 
 let asc = (a, b) => {
-        return a-b
+    return a - b
 }
 
 let desc = (a, b) => {
-    return b-a
+    return b - a
 }
 
-var toggle = 1
+let toggle = 1
 let funSort = (selection) => {
     if (toggle == 1) {
         arrayAngka[selection].sort(desc)
         funTabel(arrayAngka)
         toggle = 0
-    } 
-    else {
+    } else {
         arrayAngka[selection].sort(asc)
         funTabel(arrayAngka)
         toggle = 1
     }
-  }
+}
 
-var toggleVertical = 1
+let toggleVertical = 1
 let funSortVertical = (selection) => {
     if (toggleVertical == 1) {
-        var arrayBaru = arrayAngka.map((val) => {
+        let arrayBaru = arrayAngka.map((val) => {
             return val[selection]
         })
         arrayBaru.sort(desc)
-    
-        for (var i = 0 ; i<4 ; i++){
+
+        for (let i = 0; i < arrayAngka.length; i++) {
             arrayAngka[i][selection] = arrayBaru[i]
         }
         funTabel(arrayAngka)
         toggleVertical = 0
-    }
-    else {
-        var arrayBaru = arrayAngka.map((val) => {
+    } else {
+        let arrayBaru = arrayAngka.map((val) => {
             return val[selection]
         })
         arrayBaru.sort(asc)
-    
-        for (var i = 0 ; i<4 ; i++){
+
+        for (let i = 0; i < 4; i++) {
             arrayAngka[i][selection] = arrayBaru[i]
         }
         funTabel(arrayAngka)
@@ -55,26 +53,41 @@ let funSortVertical = (selection) => {
     }
 }
 
-let rotate = (matrix) => {          // function statement
-    const N = matrix.length - 1;   // use a constant
-    // use arrow functions and nested map;
-    const result = matrix.map((row, i) => 
-         row.map((val, j) => matrix[N - j][i])
-    );
-    matrix.length = 0;       // hold original array reference
-    matrix.push(...result);  // Spread operator
-
-    funTabel(arrayAngka)
+const flip = (matrix) => {
+    return matrix[0].map((column, index) => {
+        return matrix.map(row => row[index])
+    })
 }
 
-let rotateTimes = () => {
-    var banyak = document.getElementById("number").value
-    for (var i = 0; i<banyak ; i++){
-        rotate(arrayAngka)
+const rotateClockwise = (matrix) => {
+    let result = flip(matrix.reverse())
+    matrix.length = 0
+    matrix.push(...result)
+    funTabel(matrix)
+}
+
+const rotateCounterClockwise = (matrix) => {
+    let result = flip(matrix).reverse()
+    matrix.length = 0
+    matrix.push(...result)
+    funTabel(matrix)
+}
+
+
+let rotate = () => {
+    let arah = document.querySelector("input[name=arah]:checked").value
+    let banyak = document.getElementById("number").value
+
+    if (arah == 'Kiri') {
+        for (let i = 0; i < banyak; i++) {
+            rotateCounterClockwise(arrayAngka)
+        }
+    } else if (arah == 'Kanan') {
+        for (let i = 0; i < banyak; i++) {
+            rotateClockwise(arrayAngka)
+        }
     }
 }
-
-// var arah = document.querySelector("input[name=arah]:checked").value
 
 let funTabel = (array) => {
     let listAngka = array.map((val, index) => {
