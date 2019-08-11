fd5b5d47a64ead44ee68bd143afa83f222214746
fahrezialwi <fahrezialwi@gmail.com>
2019-08-11 22:47:15 +0700

    Question 2 solution

---
+++ challenge2.js
@@ -0,0 +1,27 @@
+let display = () => {
+    var inputNumber = document.getElementById("inputNumber").value;
+    var char = ""
+
+    for (var i = 0; i < inputNumber.length; ++i) {
+        if (inputNumber.charAt(i) == '1') {
+            char += '<span>' + "   " + '<br>' + "  |" + '<br>' + "  |" + '</span>'
+        } else if (inputNumber.charAt(i) == '2') {
+            char += '<span>' + " _ " + '<br>' + " _|" + '<br>' + "|_ " + '</span>'
+        } else if (inputNumber.charAt(i) == '3') {
+            char += '<span>' + " _ " + '<br>' + " _|" + '<br>' + " _|" + '</span>'
+        } else if (inputNumber.charAt(i) == '4') {
+            char += '<span>' + '   ' + '<br>' + "|_|" + '<br>' + "  |" + '</span>'
+        } else if (inputNumber.charAt(i) == '5') {
+            char += '<span>' + " _ " + '<br>' + "|_ " + '<br>' + " _|" + '</span>'
+        } else if (inputNumber.charAt(i) == '6') {
+            char += '<span>' + " _ " + '<br>' + "|_ " + '<br>' + "|_|" + '</span>'
+        } else if (inputNumber.charAt(i) == '7') {
+            char += '<span>' + " _ " + '<br>' + "  |" + '<br>' + "  |" + '</span>'
+        } else if (inputNumber.charAt(i) == '8') {
+            char += '<span>' + " _ " + '<br>' + "|_|" + '<br>' + "|_|" + '</span>'
+        } else if (inputNumber.charAt(i) == '9') {
+            char += '<span>' + " _ " + '<br>' + "|_|" + '<br>' + " _|" + '</span>'
+        }
+    }
+    document.getElementById("display").innerHTML = `<pre>${char}</pre>`
+}
